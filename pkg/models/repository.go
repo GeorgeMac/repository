2763870baa97652ac1fb855a6aeb6ce009a28c3e
George MacRorie <me@georgemac.com>
2019-05-27 17:39:17 +0100

    FetchedAt bug fix renamed fetched_at to fetchedAt in json tag

--- pkg/models/repository.go
+++ pkg/models/repository.go
@@ -5,5 +5,5 @@ import "time"
 type Repository struct {
 	ID        int       `json:"id"`
 	Name      string    `json:"name"`
-	FetchedAt time.Time `json:"fetched_at"`
+	FetchedAt time.Time `json:"fetchedAt"`
 }
